a1681a939af492468f4bb21014aad1d37df35f4c
ELPIDIO III CRUZ <elpidioiii.cruz@benilde.edu.ph>
2023-08-16 19:48:34 +0800

    1 - 2

---
+++ Module1/My project/Assets/TravelToDirection.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TravelToDirection : MonoBehaviour
+{
+    float movementSpeed = 5;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.position += new Vector3(0, 0, 1) * Time.deltaTime * movementSpeed;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.position += new Vector3(-1, 0, 0) * Time.deltaTime * movementSpeed;
+        }
+        
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.position += new Vector3(0, 0, -1) * Time.deltaTime * movementSpeed;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.position += new Vector3(1, 0, 0) * Time.deltaTime * movementSpeed;
+        }
+    }
+}
